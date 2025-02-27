a11af592702e5c37aefae6b90f3ab99525006807
kajetanserwecinski <kajetan.serwecinski@gmail.com>
2025-02-27 10:34:53 +0100

    Final huffman encoder

--- header.h
+++ header.h
@@ -1,5 +1,3 @@
-// Created by Kajetan Serweciński on 25/02/2025.
-
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -8,25 +6,32 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+#include <fstream>
 
 using namespace std;
 
-vector<pair<char, int>> frequencyTable(const string& input);
+#define CAPACITY 100000
+
+vector<pair<char, int> > frequencyTable(const string& input);
 
 struct Node {
     char data;
     int freq;
-    shared_ptr<Node>left, right;
+    shared_ptr<Node> left, right;
 
     Node(char c, int f) : data(c), freq(f), left(nullptr), right(nullptr) {}
 };
 
 struct Compare {
     bool operator()(const shared_ptr<Node>& lhs, const shared_ptr<Node>& rhs) {
-        return lhs -> freq > rhs -> freq;
+        return lhs->freq > rhs->freq;
     }
 };
 
-shared_ptr<Node> createHuffmanTree(const vectr<pair<car,int>>& sortedFreq);
+shared_ptr<Node> createHuffmanTree(const vector<pair<char, int> >& sortedFreq);
+
+void makeHuffmanCodes(shared_ptr<Node>& root, vector<bool>& code);
+
+unordered_map<char, vector<bool> > huffmanCodes;
 
 #endif // HEADER_H
--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ vector<pair<char, int>> frequencyTable(const string& input) {
     return sorted_freq;
 }
 
-shared_ptr<Node> createHuffmanTree(const vectr<pair<car,int>>& sortedFreq) {
+shared_ptr<Node> createHuffmanTree(const vector<pair<char, int>>& sortedFreq) {
     priority_queue<shared_ptr<Node>, vector<shared_ptr<Node>>, Compare> pq;
 
-    for (const auto& pair: sortedFreq) {
+    for (const auto& pair : sortedFreq) {
         pq.push(make_shared<Node>(pair.first, pair.second));
     }
-    while (!pq.empty()) {
+    while (pq.size() > 1) {
         shared_ptr<Node> left = pq.top(); pq.pop();
         shared_ptr<Node> right = pq.top(); pq.pop();
 
@@ -32,11 +32,70 @@ shared_ptr<Node> createHuffmanTree(const vectr<pair<car,int>>& sortedFreq) {
     return pq.top();
 }
 
-int main() {
-    const string input = "alala";
-    vector<pair<char, int>> freq = frequencyTable(input);
-    for (const auto& pair : freq) {
-        cout << pair.first << " " << pair.second << endl;
+void makeHuffmanCodes(shared_ptr<Node>& root, vector<bool>& code) {
+    if (!root) return;
+
+    if (!root->left && !root->right) {
+        huffmanCodes[root->data] = code;
     }
+
+    code.push_back(1);
+    makeHuffmanCodes(root->right, code);
+    code.pop_back();
+
+    code.push_back(0);
+    makeHuffmanCodes(root->left, code);
+    code.pop_back();
+}
+
+int main() {
+    string input = "";
+    vector<bool> code;
+    fstream f, temp;
+
+    f.open("output.bin", ios::binary | ios::out);
+    temp.open("temp.txt", ios::out); // Otwieramy temp do zapisu
+
+    char znak;
+    int i = 0;
+    while (cin.get(znak)) {
+        if (i >= CAPACITY) break;
+        temp.put(znak);
+        input += znak;
+        i++;
+    }
+
+    temp.close();
+    temp.open("temp.txt", ios::in);
+
+    vector<pair<char, int>> freq = frequencyTable(input);
+    shared_ptr<Node> root = createHuffmanTree(freq);
+    makeHuffmanCodes(root, code);
+
+    unsigned char byte = 0;
+    int bitCount = 0;
+
+    while (temp.get(znak)) {
+        const vector<bool>& huffmanCode = huffmanCodes[znak];
+        for (bool bit : huffmanCode) {
+            byte = (byte << 1) | bit;
+            bitCount++;
+
+            if (bitCount == 8) {
+                f.put(byte);
+                byte = 0;
+                bitCount = 0;
+            }
+        }
+    }
+
+    if (bitCount > 0) {
+        byte <<= (8 - bitCount);
+        f.put(byte);
+    }
+
+    temp.close();
+    f.close();
+
     return 0;
 }
